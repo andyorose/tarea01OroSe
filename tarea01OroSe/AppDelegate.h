51f5b662c78b896b67051d1426f735d7f348cfca
andyorose <andy.oros.15@gmail.com>
2013-04-11 13:18:05 -0500

    Initial Commit

---
+++ tarea01OroSe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tarea01OroSe
+//
+//  Created by SDA on 4/11/13.
+//  Copyright (c) 2013 SDA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tarea01OroSe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  tarea01OroSe
+//
+//  Created by SDA on 4/11/13.
+//  Copyright (c) 2013 SDA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
